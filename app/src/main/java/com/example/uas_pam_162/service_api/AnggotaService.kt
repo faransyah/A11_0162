86525a9858344df7eeaf677cc4f721414693b409
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2025-01-19 21:32:10 +0700

    membuat Anggota Service

---
+++ app/src/main/java/com/example/uas_pam_162/service_api/AnggotaService.kt
@@ -0,0 +1,41 @@
+package com.example.uas_pam_162.service_api
+
+import com.example.uas_pam_162.model.AllAnggotaRespon
+import com.example.uas_pam_162.model.Anggota
+import com.example.uas_pam_162.model.AnggotaDetailRespone
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface AnggotaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+
+    // Mengambil semua anggota
+    @GET("anggota")
+    suspend fun getAllAnggota(): AllAnggotaRespon
+
+    // Mengambil anggota berdasarkan ID
+    @GET("anggota/{idAnggota}")
+    suspend fun getAnggotabyId(@Path("idAnggota") idAnggota: String): AnggotaDetailRespone
+
+    // Menambahkan anggota baru
+    @POST("anggota")
+    suspend fun insertAnggota(@Body anggota: Anggota)
+
+    // Memperbarui anggota berdasarkan ID
+    @PUT("anggota/{idAnggota}")
+    suspend fun updateAnggota(@Path("idAnggota") idAnggota: String, @Body anggota: Anggota)
+
+    // Menghapus anggota berdasarkan ID
+    @DELETE("anggota/{idAnggota}")
+    suspend fun deleteAnggota(@Path("idAnggota") idAnggota: String): retrofit2.Response<Void>
+
+}
