41b3c4902e297e8912f996c1871d62888ef4ed60
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2025-01-23 19:43:20 +0700

    menambahkan variabel service untuk anggota dan peminjaman

--- app/src/main/java/com/example/uas_pam_162/Container/BukuContainer.kt
+++ app/src/main/java/com/example/uas_pam_162/Container/BukuContainer.kt
@@ -1,19 +1,27 @@
 package com.example.uas_pam_162.Container
 
+import com.example.uas_pam_162.repository.AnggotaRepository
 import com.example.uas_pam_162.repository.BukuRepository
+import com.example.uas_pam_162.repository.NetworkAnggotaRepository
 import com.example.uas_pam_162.repository.NetworkBukuRepository
+import com.example.uas_pam_162.repository.NetworkPeminjamanRepository
+import com.example.uas_pam_162.repository.PeminjamanRepository
+import com.example.uas_pam_162.service_api.AnggotaService
 import com.example.uas_pam_162.service_api.BukuService
+import com.example.uas_pam_162.service_api.PeminjamanService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 interface AppContainer{
-    val faranRepository: BukuRepository
+    val bukuRepository: BukuRepository
+    val anggotaRepository: AnggotaRepository
+    val peminjamanRepository: PeminjamanRepository
 }
 
-class BukuContainer: AppContainer{
-    private val baseUrl = "http://10.0.2.2:3010/api/buku/"
+class MainAppContainer: AppContainer{
+    private val baseUrl = "http://10.0.2.2:3010/api/"
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
@@ -23,8 +31,25 @@ class BukuContainer: AppContainer{
         retrofit.create(BukuService::class.java)
     }
 
-    override val faranRepository: BukuRepository by lazy {
+    override val bukuRepository: BukuRepository by lazy {
         NetworkBukuRepository(bukuService)
     }
+
+    private val anggotaService: AnggotaService by lazy {
+        retrofit.create(AnggotaService::class.java)
+    }
+    override val anggotaRepository: AnggotaRepository by lazy {
+        NetworkAnggotaRepository(anggotaService)
+    }
+
+    private val peminjamanService: PeminjamanService by lazy {
+        retrofit.create(PeminjamanService::class.java)
+    }
+    override val peminjamanRepository: PeminjamanRepository by lazy {
+        NetworkPeminjamanRepository(peminjamanService)
+    }
+
+
+
 }
 
