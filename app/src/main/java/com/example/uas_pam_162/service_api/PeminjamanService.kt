280faac4c11dc75f4f6dab3e8787315e770501b0
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2025-01-25 11:15:36 +0700

    membuat PeminjamanService

---
+++ app/src/main/java/com/example/uas_pam_162/service_api/PeminjamanService.kt
@@ -0,0 +1,40 @@
+package com.example.uas_pam_162.service_api
+
+import com.example.uas_pam_162.model.AllPeminjamanRespon
+import com.example.uas_pam_162.model.Peminjaman
+import com.example.uas_pam_162.model.PeminjamanDetailRespone
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface PeminjamanService {
+
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+
+    @GET("peminjaman")
+    suspend fun getAllPeminjaman(): AllPeminjamanRespon
+
+    @GET("peminjaman/{idPeminjaman}")
+    suspend fun getPeminjamanbyId(@Path("idPeminjaman")idPeminjaman: String): PeminjamanDetailRespone
+
+
+    @POST("peminjaman")
+    suspend fun insertPeminjaman(@Body peminjaman: Peminjaman)
+
+
+    @PUT("peminjaman/{idPeminjaman}")
+    suspend fun updatePeminjaman(@Path("idPeminjaman") idPeminjaman: String, @Body peminjaman: Peminjaman)
+
+
+    @DELETE("peminjaman/{idPeminjaman}")
+    suspend fun deletePeminjaman(@Path("idPeminjaman") idPeminjaman: String): retrofit2.Response<Void>
+
+}
